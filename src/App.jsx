b7f71f90ced7886b776a861fce8ed5609a87c08d
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2022-12-07 15:14:27 +0000

    First commit - layout / files removed / done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="greeting">
+        <h1 className="greeting__heading">Hello World</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
+
