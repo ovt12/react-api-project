c96da7dff6e3f7f93b3114d4f0b2f91b18624020
OLIVER TAYLOR <olivertaylor1295@hotmail.com>
2022-12-30 11:17:01 +0000

    ADDED NEW re-useable function

--- src/Components/RadioButton/RadioButton.jsx
+++ src/Components/RadioButton/RadioButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./RadioButton.scss"
+import "./RadioButton.scss";
 
 const RadioButton = ({ handleOnChange }) => {
   return (
@@ -12,3 +12,22 @@ const RadioButton = ({ handleOnChange }) => {
 };
 
 export default RadioButton;
+
+// UPDATED RADIO BUTTON PUNK API NOT RESPONSIVE SO WAIT UNTIL SO THEN IMPLEMENT 
+
+
+// const RadioButton = ({ handleOnChange }) => {
+//   const buttonData = [
+//     "High ABV - Above 6%",
+//     "Classic Range Year's Below 2010",
+//     "Acidic ph Level below 4.0",
+//   ];
+//   return (
+//     <div>
+//       {buttonData.map((button) => (
+//         <RadioButton text={button} onClick={handleOnChange} />
+//       ))}
+//     </div>
+//   );
+// };
+
